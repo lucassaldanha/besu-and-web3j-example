76627783f406f186146d5344d0d8acb32e9addd8
Lucas Saldanha <lucascrsaldanha@gmail.com>
2021-05-31 16:17:57 +1200

    Initial commit

---
+++ src/main/java/com/example/besuandweb3j/BesuAndWeb3jApplication.java
@@ -0,0 +1,13 @@
+package com.example.besuandweb3j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BesuAndWeb3jApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BesuAndWeb3jApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/besuandweb3j/BesuAndWeb3jApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.besuandweb3j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BesuAndWeb3jApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
